f98c3d8b695c7ff5e3d9b6a8645e4e1674211b22
Juan1412-dev <juan.carrizo.dev@gmail.com>
2026-01-22 23:07:12 -0300

    Configure WhiteNoise for static files

--- plataforma_cursos_natacion/settings.py
+++ plataforma_cursos_natacion/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,3 +136,4 @@ DEFAULT_FROM_EMAIL = 'no-reply@plataforma-cursos.com'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
