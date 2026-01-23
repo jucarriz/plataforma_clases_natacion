56fcc01f51fedace2f422a34d2d813e21630e5b7
Juan1412-dev <juan.carrizo.dev@gmail.com>
2026-01-22 23:35:31 -0300

    Fix static files configuration for render.

--- plataforma_cursos_natacion/settings.py
+++ plataforma_cursos_natacion/settings.py
@@ -136,4 +136,7 @@ DEFAULT_FROM_EMAIL = 'no-reply@plataforma-cursos.com'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
